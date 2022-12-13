922e55fcd5de37a9f6734a6cf29242fc1ffbe0fb
Ma Pengfei <115222128+shaoyaoqian@users.noreply.github.com>
2022-12-14 01:20:12 +0800

    [fix] 相册不允许懒加载

--- scripts/tags/lib/gallery.js
+++ scripts/tags/lib/gallery.js
@@ -2,7 +2,7 @@
  * gallery.js v1 | https://github.com/xaoxuu/hexo-theme-stellar/
  * 格式与官方标签插件一致使用空格分隔，中括号内的是可选参数（中括号不需要写出来）
  *
- * {% gallery [width:max] [effect:cards] %}
+ * {% gallery %}
  * ![img](src)
  * {% endgallery %}
  */
@@ -10,6 +10,7 @@
 'use strict'
 
 module.exports = ctx => function(args, content) {
+  // TODO : 解析这些参数，并在 main.js 文件中调用。
   args = ctx.args.map(args, ['captions', 'rowHeight', 'maxRowCount', 'margins', 'border', 'lastRow']);
   var el = '';
   var id = Math.random().toString(36).slice(2);
@@ -18,7 +19,8 @@ module.exports = ctx => function(args, content) {
     imgs = imgs.match(/<img(.*?)src="(.*?)"(.*?)>/gi);
     if (imgs && imgs.length > 0) {
       imgs.forEach((img, i) => {
-        // img = img.replace('<img src', '<img src');
+        // WARN: 不应当懒加载，需要获取图片长宽信息。
+        img = img.replace('<img src', '<img no-lazy src');
         var caption = img.match(/\salt=['"](.*?)['"]/)[1];
         var href = img.match(/\ssrc=['"](.*?)['"]/)[1];//.slice(5,-1);
         el += '<a data-fancybox="' + id + '" data-caption="' + caption + '" href="' + href + '">';
